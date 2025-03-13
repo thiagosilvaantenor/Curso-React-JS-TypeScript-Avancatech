06b950595ce6add1519ca81866fdcfe0306b318c
Thiago Silva Antenor <thiagoantenor31@gmail.com>
2025-03-13 10:22:25 -0300

    feat: exercício 5 use state

---
+++ 05-estados/src/App.jsx
@@ -0,0 +1,14 @@
+import Texto from "./components/Texto";
+
+const App = () => {
+  return (
+    <div>
+      <Texto cor={"purple"} />
+      <Texto cor={"blue"} />
+      <Texto cor={"gold"} />
+      <Texto cor={"red"} />
+
+    </div>
+  )
+}
+export default App;
---
+++ 05-estados/src/components/Texto/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const Texto = ({ cor }) => {
+    const [texto, setTexto] = useState("Texto Inicial")
+    const [entradaTexto, setEntradaTexto] = useState("")
+
+    function clicou() {
+        setTexto(entradaTexto);
+    }
+
+    return (
+        <div>
+            <h1 style={{ color: cor }}> {texto} </h1>
+
+            <input
+                value={entradaTexto}
+                onChange={(e) => {
+                    setEntradaTexto(e.target.value)
+                }}
+            />
+
+            <button onClick={clicou} >Mudar</button>
+
+        </div>
+    )
+}
+export default Texto
